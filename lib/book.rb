17da6cef90ce28d7002957f3f910c1d17005d58d
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-29 16:09:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,31 +1,11 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
   def initialize(title)
-    @tite = title
-  end
-  
-  def title
-    @title
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count
-    @page_count
-  end
-  def genre=(genre)
-    @genre = genre
+    @title = title
   end
  
-  def genre
-    @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
